913cc65d1d030231a52e51d34b03aa3769f80f7d
akxaalex16 <akxaalex16@gmail.com>
2024-07-24 21:20:03 -0700

    update main.py
    checked to see if the player has enough money to continue betting

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import random
+
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
 
 def deposit():
     while True:
@@ -50,8 +63,14 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
 
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
     print(balance, lines)
