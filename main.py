cf14a74d8eae1832bec6e787d4b1094949c6fa6d
akxaalex16 <akxaalex16@gmail.com>
2024-07-31 21:53:58 -0700

    Added spin function to continue betting and playing the game
    moved code from main() to spin()

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 
-
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
@@ -115,8 +114,7 @@ def get_bet():
     return amount
 
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -135,5 +133,19 @@ def main():
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}. ")
     print(f"You won on lines: ", *winning_lines)
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f'Current balance is ${balance}')
+        spins = input('Press enter to play. (q to quit) ')
+        if spins == "q":
+            break
+        balance += spin(balance)
+
+    print(f'You left with ${balance}')
+
 
 main()
